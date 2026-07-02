01815dffaf56c30d770b670cfb51e64b765eb6c6
natzillax <nantharat.nat@gmail.com>
2026-07-02 14:30:12 +0700

    Update home page redirection

--- app/page.tsx
+++ app/page.tsx
@@ -2,5 +2,5 @@ import { redirect } from 'next/navigation';
 
 export default function RootPage() {
   // สั่งให้รีไดเรกต์ไปที่หน้าแอดมินหอพักทันที
-  redirect('/landlord');
+  redirect('/login');
 }
